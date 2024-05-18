4e6c655ff0b0822ddaa6fcfe4f90dd01d17c37f4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-17 22:38:23 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("----QuenteFrio----");
+Console.WriteLine("\nTente acertar o número sorteado pelo jogo de 1 A 100");
+Console.WriteLine("Você terá 7 tentativas para acertar!\n");
+
+int sorteado;
+sorteado = RandomNumberGenerator.GetInt32(1, 101);
+Console.WriteLine($"{sorteado}");
+
+Console.Write("De seu palpite...: ");
+int palpite = Convert.ToInt32(Console.ReadLine());
+
+int erro = palpite - sorteado;
+int distanciaErro = Math.Abs(erro);
+
+// Console.WriteLine($"{distanciaErro}");
+
+// while(palpite == sorteado)
+// {
+//     Console.Write("De seu palpite...: ");
+// palpite = Convert.ToInt32(Console.ReadLine());
+
+// int erro = palpite - sorteado;
+// int distanciaErro = Math.Abs(erro);
+
+// Console.WriteLine($"{distanciaErro}");
+// }
